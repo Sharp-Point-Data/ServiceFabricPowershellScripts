bd0d9c5147141d23a437cfb8672b766218a798d0
Michael Yabko <59970024+MichaelYabko@users.noreply.github.com>
2021-11-19 08:36:26 -0500

    Update prepare_vm_disk.ps1

--- prepare_vm_disk.ps1
+++ prepare_vm_disk.ps1
@@ -22,7 +22,20 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-$disk = Get-Disk -FriendlyName "data"
+$disks = Get-Disk | Sort Number
+# 83..89 = S..Y
+$letters = 83..89 | ForEach-Object { [char]$_ }
+$count = 0
+$label = "datadisk"
 
-New-Partition -UseMaximumSize -DriveLetter "G"
-Format-Volume -FileSystem NTFS -NewFileSystemLabel "Mounted Drive" -Confirm:$false -Force
+for($index = 2; $index -lt $disks.Count; $index++) {
+    $driveLetter = $letters[$count].ToString()
+    if ($disks[$index].partitionstyle -eq 'raw') {
+        $disks[$index] | Initialize-Disk -PartitionStyle MBR -PassThru |
+            New-Partition -UseMaximumSize -DriveLetter $driveLetter |
+            Format-Volume -FileSystem NTFS -NewFileSystemLabel "$label.$count" -Confirm:$false -Force
+    } else {
+        $disks[$index] | Get-Partition | Set-Partition -NewDriveLetter $driveLetter
+    }
+    $count++
+}
